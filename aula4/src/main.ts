e8a119a114d323db78f0aca62222a8de11c9a34d
Gabriela <gabrielamrs19@gmail.com>
2024-10-09 21:54:02 -0300

    first commit

---
+++ aula4/src/main.ts
@@ -0,0 +1,18 @@
+import './style.css'
+
+const campoTexto = document.getElementById('campo-texto') as HTMLInputElement 
+const botao = document.getElementById('button') as HTMLButtonElement
+const lista = document.getElementById('lista') as HTMLDivElement
+
+console.log(campoTexto)
+botao?.addEventListener('click', function(){
+    const texto = campoTexto?.value;
+    console.log(texto);
+
+    const paragrafo = document.createElement('p');
+    paragrafo.innerHTML = texto;
+    lista?.appendChild(paragrafo)
+})
+
+localStorage.setItem('@TEXTO', lista)
+
